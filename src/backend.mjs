080fd903410da563ba84c3a96befec7136aee73c
Juillard Mathis <mathis.juillard@edu.univ-fcomte.fr>
2025-01-31 14:31:12 +0100

    remplacer les donner statiques

---
+++ src/backend.mjs
@@ -0,0 +1,18 @@
+import PocketBase from 'pocketbase';
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function getOffres() {
+    try {
+        let data = await pb.collection('Agence').getFullList({
+            sort: '-created',
+        });
+        data = data.map((offres) => {
+            offres.img = pb.files.getURL(offres, offres.images);
+            return offres;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des agences', error);
+        return [];
+    }
+}
